b0dc2db4bce65f1474d779ace09fc663bf4ee346
pianojay <159106973+pianojay@users.noreply.github.com>
2024-07-03 04:48:18 +0900

    More skeletons for BLDC, Servo, PID, and other essential stuff

--- main/main.ino
+++ main/main.ino
@@ -1,7 +1,8 @@
 #include <SoftwareSerial.h>  // for Bluetooth
-#include <Servo.h>           // for BLDC and servo
-#include <Wire.h>            // for MPU6050
-#include <MPU6050.h>         // for MPU6050; https://github.com/ElectronicCats/mpu6050
+#include <PID_v1.h>
+#include <Servo.h>    // for BLDC and servo
+#include <Wire.h>     // for MPU6050
+#include <MPU6050.h>  // for MPU6050; https://github.com/ElectronicCats/mpu6050
 
 // Based on Uno but surely compatible with Nano.
 
@@ -9,22 +10,46 @@
 const byte rxPin = 2;
 const byte txPin = 3;
 SoftwareSerial BTS(rxPin, txPin);
-int IByte = 0;
+int IByte;
 
-// BLDC motor; Create PWM signal via Servo.h for ESC
+// BLDC motor; Create PWM signal via Servo.h for ESC (https://joyonclear.tistory.com/9)
 const byte ringPin = 4;
 Servo ring;
-int ringinit = 0;
-int kv = 1000;  // change this per motor spec
+float ringv = 1500, ringvs = 1500;  // current, desired
+float factor = 0.1;                 // Manual Tuning
+#define MinRingv 1000               // based on PWM signal
+#define MaxRingv 2000
+
+void ringvsmoother() {  // exponential moving average
+  ringv = ringv * (1.0 - factor) + ringvs * factor;
+}
 
 // Servo motor: DS3218MG
 const byte armPin = 5;
 Servo arm;
-int arminit = 0;
+double rolli, armao = 90, rolls = 0;           // current roll, servo angle out, desired roll
+double armkp = 1, armki = 0.05, armkd = 0.25;  // Manual Tuning
+PID armPid(&rolli, &armao, &rolls, armkp, armki, armkd, DIRECT);
+#define MinArma 0  // based on servo
+#define MaxArma 180
+#define Minrolls -45  // manual tuning
+#define Maxrolls 45
 
-// MPU6050; Check MPU6050BasicExample.ino
-const byte mpuPin = 6;
+// MPU6050; Check MPU6050IMU.ino
 MPU6050lib mpu;
+// Not implemented here
+
+bool isfallen = false;
+
+void checkfall() {
+  // if roll from IMU over threshold then isfallen = true
+  // -> try to stand up at next cycle
+}
+
+void standup() {
+  // Do something
+}
+
 
 void setup() {
   BTS.begin(9600);
@@ -32,26 +57,63 @@ void setup() {
     ;
   BTS.println("Connected!");
 
+  ring.writeMicroseconds(ringv);
   ring.attach(ringPin);
-  ring.write(ringinit);
 
+  arm.write(armao);
+  armPid.SetMode(AUTOMATIC);
+  armPid.SetOutputLimits(MinArma, MaxArma);
   arm.attach(armPin);
-  arm.write(arminit);
+
+  // And then MPU6050 setup
 }
 
 void loop() {
+  // MPU6050 stuff; -> get roll
+  // rolli = roll;
+
+  checkfall();
+  if (isfallen) {
+    BTS.println("");
+    BTS.println("I have fallen.");
+    BTS.println("Initiating standing up protocol");
+    standup();
+  }
+
   if (BTS.available()) {
     IByte = BTS.read();
-    if (IByte == 'w') {
-      BTS.println("Forward");
-    } else if (IByte == 's') {
-      BTS.println("Backward");
-    } else if (IByte == 'a') {
-      BTS.println("Turn Left");
-    } else if (IByte == 'd') {
-      BTS.println("Turn Right");
-    } else if (IByte == ' ') {
-      BTS.println("Reset");
+    switch (IByte) {
+      case ' ':
+        BTS.println("Reset");
+        ringvs = 0;
+        rolls = 0;
+        break;
+      case 'w':
+        BTS.println("Forward");
+        ringvs += 5;
+        break;
+      case 's':
+        BTS.println("Backward");
+        ringvs -= 5;
+        break;
+      case 'a':
+        BTS.println("Roll Left");
+        rolls -= 5;
+        break;
+      case 'd':
+        BTS.println("Roll Right");
+        rolls += 5;
+        break;
+      default:
+        break;
     }
+    ringvs = constrain(ringvs, MinRingv, MaxRingv);
+    ringvsmoother();
+    ring.writeMicroseconds(ringv);
+    rolls = constrain(rolls, Minrolls, Maxrolls);
+    // armPid.SetTunings(,,);
+    // for interactive tuning?
+    armPid.Compute();
+    arm.write(armao);
   }
 }
