a9da9e72afa2516582345f0846d7b763d8364949
pianojay <159106973+pianojay@users.noreply.github.com>
2024-11-24 19:52:14 +0900

    Update README.md

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 # MR_2024_Ringbot
-MR 앞구루기 로봇
+MR 2024 정기프로젝트 5조  압 구루 기로 봇
 
-MR24 정기프로젝트 5조:
+멘토: 명경민, 서정근, 임준범
 
+프로젝트원: 김민재, 김재욱, 김준열, 유호현, 문우혁, 박찬, Arslan, Marino, Nijat
+
+파일 (사진, 영상, 문서 등)
 https://drive.google.com/drive/folders/1JUdwt68mXYVQRw85nZ2OcbMQ8bC_Edo1
 
+CAD (onshape)
 https://cad.onshape.com/documents/21efd9558f1548b050d4c0d5/w/83893a8d301272236c2930e6/e/518928168926469dfc207ae3
--- main_esp32_c3/test2/test2.ino
+++ main_esp32_c3/test2/test2.ino
@@ -1,8 +1,17 @@
 #include "ESP32Servo.h"
 
-#define arm1Pin 4  // left
-#define arm2Pin 8  // right
+#define arm1Pin 4  // 4 = left
+#define arm2Pin 8  // 8 = right
 Servo arm1, arm2;
+double rolli, armao = 0, rolls = 0;             // current roll (sensor), servo PWM, desired roll
+double armkp = 30.0, armki = 0.0, armkd = 0.0;  // unit: percent. TODO: Manual Tuning
+#define Res 100
+#define ArmaDev 90    // Deviation; Determine the actual servo PWM from armao
+#define ArmWidth 10   // To compensate each arm's thickness
+#define MinRolls -45  // To prevent excessive tilting
+#define MaxRolls 45
+#define Compensate 0   // angle exceeding of arm1 from arm2
+#define Compensate2 4  // angle to stand upright when symmetric
 
 void setup() {
   Serial.begin(9600);
@@ -11,24 +20,12 @@ void setup() {
   arm1.attach(arm1Pin, 500, 2500);
   arm2.attach(arm2Pin, 500, 2500);
   arm1.write(75);
-  arm2.write(180-75);
+  arm2.write(180 - 75);
 }
 
 int i = 0;
 
 void loop() {
-  delay(2000);
-  for (; i <= 45; i += 1) {
-    arm1.write(i);
-    arm2.write(180 - i);
-    Serial.println(i);
-    delay(100);
-  }
-  delay(2000);
-  for (; i >= 25; i -= 1) {
-    arm1.write(i);
-    arm2.write(180 - i);
-    Serial.println(i);
-    delay(100);
-  }
+  arm1.write(90 - Compensate - rolls + Compensate2);
+  arm2.write(180 - 90 - rolls + Compensate2);
 }
